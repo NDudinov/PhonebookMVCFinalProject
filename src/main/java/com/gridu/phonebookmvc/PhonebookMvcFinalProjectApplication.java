f5af5cc97761f237c0207a328e8e269183888d67
DuduinovN <C@nGri$5800>
2021-07-06 03:36:47 +0300

    Added spring start files

---
+++ src/main/java/com/gridu/phonebookmvc/PhonebookMvcFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gridu.phonebookmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonebookMvcFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonebookMvcFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gridu/phonebookmvc/PhonebookMvcFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridu.phonebookmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhonebookMvcFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
